d91164c5006961bcac4ba565ac12f62135e6a66e
Roy Bachar <roybcx@proton.me>
2022-11-24 03:13:06 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use bevy::prelude::{App, Commands, Component, Plugin, Query, Res, ResMut, Resour
 use bevy::time::{Time, Timer, TimerMode};
 use bevy::DefaultPlugins;
 
-//  NOTE:
+#[derive(Component)]
+struct Person;
+
+// NOTE:
 // Since other entities might have names too,
 // it often makes sense to break datatypes up into
 // small pieces to encourage code reuse.
-
-#[derive(Component)]
-struct Person;
 
 #[derive(Component)]
 struct Name(String);
@@ -21,6 +21,12 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         let gtimer = Timer::from_seconds(2.0, TimerMode::Once);
+
+        // NOTE:
+        // Startup systems are just like normal systems,
+        // but they run exactly once, before all other
+        // systems, right when our app starts.
+
         app.insert_resource(GreetTimer(gtimer))
            .add_startup_system(add_people)
            .add_system(hello_world)
@@ -28,25 +34,23 @@ impl Plugin for HelloPlugin {
     }
 }
 
-//  NOTE:
-// Startup systems are just like normal systems,
-// but they run exactly once, before all other
-// systems, right when our app starts.
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Harvey Specter".to_string())));
 }
 
-//  NOTE:
+// NOTE:
 //  The parameters we pass in to a "system function"
 //  define what data the system runs on. In this case,
 //  greet_people will run on all
 //  entities with the Person and Name component!.
+
 fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    //  INFO:
+    // INFO:
     // update our timer with the time elapsed
     // since the last update. if that caused the timer to finish,
     // we say hello to everyone.
+
     if timer.0.tick(time.delta()).just_finished() {
         for name in query.iter() {
             let name = &name.0;
@@ -55,11 +59,12 @@ fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Na
     }
 }
 
-//  NOTE:
-// "hello world!" might show up in a different order
-// than expected. This is because systems run in parallel
-// by default whenever possible.
 fn hello_world() {
+    // NOTE:
+    // "hello world!" might show up in a different order
+    // than expected. This is because systems run in parallel
+    // by default whenever possible.
+
     println!("Hello, world!");
 }
 
